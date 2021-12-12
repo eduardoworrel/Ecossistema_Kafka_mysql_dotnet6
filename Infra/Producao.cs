eaa20873e2d841ffcbd6c2b667790c823d3a1f08
Eduardo Worrel <eduardo.worrel@gmail.com>
2021-12-12 19:47:41 -0300

    Ajudante mysql

---
+++ Infra/Producao.cs
@@ -0,0 +1,24 @@
+using System;
+using Newtonsoft.Json;
+namespace DAL
+{
+
+    public class Producao
+    {
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonRequired]
+        [JsonProperty("Id_Gerador")]
+        public int Id_Gerador { get; set; }
+
+        [JsonRequired]
+        [JsonProperty("Datahora_Captura")]
+        public DateTime Datahora_Captura { get; set; }
+
+        [JsonRequired]
+        [JsonProperty("Valor")]
+        public string? Valor { get; set; }
+    }
+
+}
