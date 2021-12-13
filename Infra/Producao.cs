859d15c329211e379286b1edbf0d4fa2ba870229
Eduardo Worrel <eduardo.worrel@gmail.com>
2021-12-13 09:46:10 -0300

    limpando propriedades json

--- Infra/Producao.cs
+++ Infra/Producao.cs
@@ -1,23 +1,13 @@
 using System;
 using Newtonsoft.Json;
-namespace DAL
+namespace Infra
 {
 
     public class Producao
     {
-        [JsonProperty("Id")]
         public int Id { get; set; }
-
-        [JsonRequired]
-        [JsonProperty("Id_Gerador")]
         public int Id_Gerador { get; set; }
-
-        [JsonRequired]
-        [JsonProperty("Datahora_Captura")]
         public DateTime Datahora_Captura { get; set; }
-
-        [JsonRequired]
-        [JsonProperty("Valor")]
         public string? Valor { get; set; }
     }
 
