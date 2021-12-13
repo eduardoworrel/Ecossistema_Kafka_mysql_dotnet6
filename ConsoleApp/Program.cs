5b8d3d4de2ca7bee02a12390e3dc7b2a48a82ac8
Eduardo Worrel <eduardo.worrel@gmail.com>
2021-12-13 09:45:33 -0300

    Removendo metodo Main, não obrigatório no dotnet 6

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,73 +4,67 @@ using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Newtonsoft.Json;
-using DAL;
+using Infra;
 
 
-namespace ConsoleApp
+
+string bootstrapServers = "localhost:9092";
+string nomeTopic = "topic1";
+string groupId = "group1";
+
+Console.Write($"BootstrapServers = {bootstrapServers}");
+Console.Write($"Topic = {nomeTopic}");
+Console.Write($"Group Id = {groupId}");
+
+var config = new ConsumerConfig
 {
-    class Program
+    BootstrapServers = bootstrapServers,
+    GroupId = groupId,
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+try
+{
+    using (var consumer = new ConsumerBuilder<Ignore, string>(config)
+    .Build())
     {
-        static void Main(string[] args)
+        consumer.Subscribe(nomeTopic);
+        try
         {
-            string bootstrapServers = "localhost:9092";
-            string nomeTopic = "topic1";
-            string groupId = "group1";
-
-            Console.Write($"BootstrapServers = {bootstrapServers}");
-            Console.Write($"Topic = {nomeTopic}");
-            Console.Write($"Group Id = {groupId}");
-
-            var config = new ConsumerConfig
+            while (true)
             {
-                BootstrapServers = bootstrapServers,
-                GroupId = groupId,
-                AutoOffsetReset = AutoOffsetReset.Earliest
-            };
-
-            CancellationTokenSource cts = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, e) =>
-            {
-                e.Cancel = true;
-                cts.Cancel();
-            };
-
-            try
-            {
-                using (var consumer = new ConsumerBuilder<Ignore, string>(config)
-                .Build())
-                {
-                    consumer.Subscribe(nomeTopic);
-                    try
-                    {
-                        while (true)
-                        {
-                            var cr = consumer.Consume(cts.Token);
-                            Save(Newtonsoft.Json.JsonConvert.DeserializeObject<Producao>(cr.Message.Value));
-                            Console.Write("Registro salvo com sucesso!")
-                        }
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        consumer.Close();
-                        Console.Write("Cancelada a execução do Consumer...");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.Write($"Exceção: {ex.GetType().FullName} | " +
-                            $"Mensagem: {ex.Message}");
+                var cr = consumer.Consume(cts.Token);
+                Save(Newtonsoft.Json.JsonConvert.DeserializeObject<Producao>(cr.Message.Value));
+                Console.Write("Registro salvo com sucesso!")
             }
         }
-
-        static void Save(Producao producao)
+        catch (OperationCanceledException)
         {
-            using (var db = new MyDbContext())
-            {
-                db.Producoes.Add(producao);
-                db.SaveChanges();
-            }
+            consumer.Close();
+            Console.Write("Cancelada a execução do Consumer...");
         }
     }
 }
+catch (Exception ex)
+{
+    Console.Write($"Exceção: {ex.GetType().FullName} | " +
+                $"Mensagem: {ex.Message}");
+}
+        
+
+static void Save(Producao producao)
+{
+    using (var db = new MyDbContext())
+    {
+        db.Producoes.Add(producao);
+        db.SaveChanges();
+    }
+}
+ 
